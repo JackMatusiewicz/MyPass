0ad7861142c55c3c7ae23d6c198ce088c381cefd
Jack Matusiewicz <jack.matusiewicz@outlook.com>
2017-09-11 00:14:49 +0100

    Adding HKDF.

---
+++ MyPass/Hkdf.fs
@@ -0,0 +1,28 @@
+﻿module Hkdf
+
+open System.Security.Cryptography;
+open System.IO;
+open System;
+
+let expand (initialKeyData : byte[]) (salt : byte[]) (info : byte[]) (length : int) : byte[] =
+    use hmac = new HMACSHA256(salt)
+    let hmacKey = hmac.ComputeHash(initialKeyData)
+    use hmac = new HMACSHA256(hmacKey)
+
+    let rec expandKey (result : byte[]) (previousResultBlock : byte[]) (remainingBytes : int) (i : int) =
+        match remainingBytes with
+        | _ when remainingBytes < 0 -> result
+        | _ ->
+            let ci = Array.create (previousResultBlock.Length + info.Length + 1) (byte 0)
+            System.Array.Copy(previousResultBlock, 0, ci, 0, previousResultBlock.Length)
+            System.Array.Copy(info, 0, ci, previousResultBlock.Length, info.Length)
+            ci.[ci.Length - 1] <- (byte i)
+
+            let resultBlock = hmac.ComputeHash(ci);
+            Array.Copy(resultBlock, 0, result, length - remainingBytes, Math.Min(remainingBytes, resultBlock.Length))
+            let newRemaining = remainingBytes - resultBlock.Length
+            expandKey result resultBlock newRemaining (i+1)
+
+    let result = Array.create length (byte 0)
+    let prev = Array.create 0 (byte 0)
+    expandKey result prev length 1
