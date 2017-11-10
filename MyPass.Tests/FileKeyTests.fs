e3b4546ae1f00e6e379e3d74e98806df869bfa67
JackMatusiewicz <jack.matusiewicz@gmail.com>
2017-11-10 07:29:33 +0000

    Adding file key test to ensure it has correct properties

---
+++ MyPass.Tests/FileKeyTests.fs
@@ -0,0 +1,19 @@
+﻿namespace MyPass.Tests
+
+open NUnit.Framework
+open MyPass
+open Result
+
+module FileKeyTests =
+
+    [<Test>]
+    [<Repeat(10000)>]
+    let ``When a file key is generated then it has the correct properties`` () =
+        let (FileKey key) = FileKey.generateFileKey()
+        Assert.That(key.Length, Is.EqualTo(16))
+        let containsCorrectCharacters =
+                Array.fold
+                    (fun b c -> b && (Array.contains c FileKey.availableCharacters))
+                    true
+                    (key.ToCharArray())
+        Assert.That(containsCorrectCharacters, Is.True)
