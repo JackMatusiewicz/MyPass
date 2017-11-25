f0b3ccd7bc926ba83263f1a65d148a9fbeb8874f
JackMatusiewicz <jack.matusiewicz@gmail.com>
2017-11-25 16:07:37 +0000

    Adding more master key tests

--- MyPass.Tests/PasswordTests.fs
+++ MyPass.Tests/PasswordTests.fs
@@ -17,3 +17,51 @@ module PasswordTests =
         let pwOne = Password.createMasterPassword versionId masterPassPhrase secretKey userId
         let pwTwo = Password.createMasterPassword versionId masterPassPhrase secretKey userId
         Assert.That(pwOne.SequenceEqual(pwTwo), Is.True)
+
+    [<Test>]
+    [<Repeat(5)>]
+    let ``When recreating the password with the same parameters but different secretKey then the passwords differ`` () =
+        let userId = Password.createPassword 15u
+        let versionId = Password.createPassword 7u
+        let masterPassPhrase = Password.createPassword 10u
+        let secretKey = Array.create 10 (byte 0)
+        let secretKey2 = Array.create 10 (byte 1)
+        let pwOne = Password.createMasterPassword versionId masterPassPhrase secretKey userId
+        let pwTwo = Password.createMasterPassword versionId masterPassPhrase secretKey2 userId
+        Assert.That(pwOne.SequenceEqual(pwTwo), Is.False)
+
+    [<Test>]
+    [<Repeat(5)>]
+    let ``When recreating the password with the same parameters but different userId then the passwords differ`` () =
+        let userId = Password.createPassword 15u
+        let userId2 = Password.createPassword 15u
+        let versionId = Password.createPassword 7u
+        let masterPassPhrase = Password.createPassword 10u
+        let secretKey = Array.create 10 (byte 0)
+        let pwOne = Password.createMasterPassword versionId masterPassPhrase secretKey userId
+        let pwTwo = Password.createMasterPassword versionId masterPassPhrase secretKey userId2
+        Assert.That(pwOne.SequenceEqual(pwTwo), Is.False)
+
+    [<Test>]
+    [<Repeat(5)>]
+    let ``When recreating the password with the same parameters but different versionIds then the passwords differ`` () =
+        let userId = Password.createPassword 15u
+        let versionId = Password.createPassword 7u
+        let versionId2 = Password.createPassword 7u
+        let masterPassPhrase = Password.createPassword 10u
+        let secretKey = Array.create 10 (byte 0)
+        let pwOne = Password.createMasterPassword versionId masterPassPhrase secretKey userId
+        let pwTwo = Password.createMasterPassword versionId2 masterPassPhrase secretKey userId
+        Assert.That(pwOne.SequenceEqual(pwTwo), Is.False)
+
+    [<Test>]
+    [<Repeat(5)>]
+    let ``When recreating the password with the same parameters but different passPhrases then the passwords differ`` () =
+        let userId = Password.createPassword 15u
+        let versionId = Password.createPassword 7u
+        let masterPassPhrase = Password.createPassword 10u
+        let masterPassPhrase2 = Password.createPassword 10u
+        let secretKey = Array.create 10 (byte 0)
+        let pwOne = Password.createMasterPassword versionId masterPassPhrase secretKey userId
+        let pwTwo = Password.createMasterPassword versionId masterPassPhrase2 secretKey userId
+        Assert.That(pwOne.SequenceEqual(pwTwo), Is.False)
