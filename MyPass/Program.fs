9963f1712f028c712e13e36e253059382c187ac2
JackMatusiewicz <jack.matusiewicz@gmail.com>
2018-04-24 22:10:14 +0100

    Adding args parsing for prototype app

--- MyPass/Program.fs
+++ MyPass/Program.fs
@@ -1,5 +1,6 @@
 ﻿namespace MyPass
 
+open Argu
 open Aes
 open Password
 open System.Text
@@ -7,11 +8,37 @@ open Vault
 open ConsoleUi
 open Result
 
+type Arguments =
+    | Mode of mode : string
+    interface IArgParserTemplate with
+        member x.Usage =
+            match x with
+            | Mode _ -> "Specify the mode you wish to run, choose from: CREATE|ADD|LIST|GET"
+
 module Main =
 
     [<EntryPoint>]
-    let main _ =
-        ConsoleUi.createNewVault ()
-        ConsoleUi.addSecret ()
-        ConsoleUi.listSecrets () |> ignore
+    let main args =
+        let argsParser = ArgumentParser.Create<Arguments>(programName = "MyPass")
+        let helpSpecified =
+            args
+            |> Array.toList
+            |> List.map (fun s -> s.ToLower())
+            |> List.filter (fun s -> s = "--help")
+            |> List.length
+            |> fun len -> len > 0
+
+        match helpSpecified with
+        | true -> argsParser.PrintUsage () |> ignore
+        | false ->
+            let parsedArgs : ParseResults<Arguments> = argsParser.Parse args
+            match parsedArgs.Contains Mode with
+            | false -> argsParser.PrintUsage () |> ignore
+            | true ->
+                let mode = (parsedArgs.GetResult Mode).ToLower()
+                match mode with
+                | "create" -> ConsoleUi.createNewVault ()
+                | "add" -> ConsoleUi.addSecret ()
+                | "list" -> ConsoleUi.listSecrets ()
+                | _ -> argsParser.PrintUsage () |> ignore
         0
