d5de4f2c14d6cc9a894e1c63a33b937c3d30588e
JackMatusiewicz <jack.matusiewicz@gmail.com>
2018-05-04 05:31:15 +0100

    Forcing passwords to be different if the test requires it

--- MyPass.Tests/PasswordTests.fs
+++ MyPass.Tests/PasswordTests.fs
@@ -7,6 +7,13 @@ open System.Linq
 
 module PasswordTests =
 
+    let rec private generateDifferentPassword (pw : string) (genPass : unit -> string) : string =
+        let len = pw.Length
+        let newPass = genPass ()
+        match newPass = pw with
+        | true -> generateDifferentPassword pw genPass
+        | false -> newPass
+
     [<Test>]
     [<Repeat(50)>]
     let ``When constructing password with extra chars, then no unspecified chars are used `` () =
@@ -45,7 +52,7 @@ module PasswordTests =
     [<Repeat(5)>]
     let ``When recreating the password with the same parameters but different userId then the passwords differ`` () =
         let userId = Password.createPassword 15u
-        let userId2 = Password.createPassword 15u
+        let userId2 = generateDifferentPassword userId (fun () -> Password.createPassword 15u)
         let versionId = Password.createPassword 7u
         let masterPassPhrase = Password.createPassword 10u
         let secretKey = Array.create 10 (byte 0)
@@ -58,7 +65,7 @@ module PasswordTests =
     let ``When recreating the password with the same parameters but different versionIds then the passwords differ`` () =
         let userId = Password.createPassword 15u
         let versionId = Password.createPassword 7u
-        let versionId2 = Password.createPassword 7u
+        let versionId2 = generateDifferentPassword versionId (fun () -> Password.createPassword 7u)
         let masterPassPhrase = Password.createPassword 10u
         let secretKey = Array.create 10 (byte 0)
         let pwOne = Password.createMasterPassword versionId masterPassPhrase secretKey userId
@@ -71,7 +78,7 @@ module PasswordTests =
         let userId = Password.createPassword 15u
         let versionId = Password.createPassword 7u
         let masterPassPhrase = Password.createPassword 10u
-        let masterPassPhrase2 = Password.createPassword 10u
+        let masterPassPhrase2 = generateDifferentPassword versionId (fun () -> Password.createPassword 10u)
         let secretKey = Array.create 10 (byte 0)
         let pwOne = Password.createMasterPassword versionId masterPassPhrase secretKey userId
         let pwTwo = Password.createMasterPassword versionId masterPassPhrase2 secretKey userId
