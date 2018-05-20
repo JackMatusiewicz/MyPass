368e96cdd7e5bdf7f7559695960303e59be0f87a
JackMatusiewicz <jack.matusiewicz@gmail.com>
2018-05-20 18:50:43 +0100

    Removing unused functions

--- MyPass/Tuple.fs
+++ MyPass/Tuple.fs
@@ -2,11 +2,5 @@
 
 module Tuple =
 
-    let bimap (f : 'a -> 'b) (g : 'c -> 'd) (a,c) =
-        (f a, g c)
-
     let map (f : 'a -> 'b) ((c,a) : 'c * 'a) =
         (c, f a)
-
-    let leftMap (f : 'a -> 'b) ((a,c) : 'a * 'c) =
-        (f a, c)
