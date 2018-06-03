04e364ee57f411516bb167a6c455932d111f884c
JackMatusiewicz <jack.matusiewicz@gmail.com>
2018-06-03 14:48:19 +0100

    AesKey is now only in memory decrypted for as long as it has to be

--- MyPass/Aes.fs
+++ MyPass/Aes.fs
@@ -28,12 +28,22 @@ module Aes =
         aes.KeySize <- keySizeBits
         aes
 
+    let private decryptKeyBytes (k : AesKey) =
+        let keyBytes = k.Key
+        ProtectedData.Unprotect (keyBytes, null, DataProtectionScope.CurrentUser)
+
+    let private zeroKey (bytes : byte[]) =
+        for i in 0 .. (bytes.Length - 1) do bytes.[i] <- (byte 0)
+
     let makeFrom (bytes : byte[]) : AesKey =
             match bytes.Length = keySizeBytes with
             | true ->
+                let bytes =
+                    Array.copy bytes
+                    |> fun d -> ProtectedData.Protect (d, null, DataProtectionScope.CurrentUser)
                 { Key = bytes }
             | false ->
-                invalidArg "bytes" "Invalid length of key"  
+                invalidArg "bytes" "Invalid length of key"
 
     let make () =
         use aes = makeKey ()
@@ -46,10 +56,10 @@ module Aes =
         let keyBytes = deriver.GetBytes(keySizeBytes)
         {Key = keyBytes}
 
-    let private createEncryptionStream (key : AesKey) (data : Stream) : CryptoStream =
+    let private createEncryptionStream (key : byte[]) (data : Stream) : CryptoStream =
         use aes = makeKey ()
         aes.GenerateIV()
-        aes.Key <- key.Key
+        aes.Key <- key
         let encryptor = aes.CreateEncryptor(aes.Key, aes.IV)
         let cs = new CryptoStream(data, encryptor, CryptoStreamMode.Write)
         use bw = new BinaryWriter(cs, System.Text.Encoding.UTF8, true)
@@ -57,6 +67,7 @@ module Aes =
         cs
 
     let encrypt (data : byte[]) (key : AesKey) : byte[] =
+        let key = decryptKeyBytes key
         let writeDataToStream (data : byte[]) : MemoryStream = 
             let ms = new MemoryStream()
             use encryptionStream = createEncryptionStream key ms
@@ -71,21 +82,24 @@ module Aes =
         let outputDataStream = writeDataToStream data
         let cipherText = outputDataStream.ToArray()
         outputDataStream.Dispose()
+        zeroKey key
         cipherText
 
-    let private createDecryptionStream (key : AesKey) (data : Stream) : CryptoStream =
+    let private createDecryptionStream (key : byte[]) (data : Stream) : CryptoStream =
         use aes = makeKey ()
         aes.GenerateIV()
-        aes.Key <- key.Key
+        aes.Key <- key
         let ivBytes = Array.create (aes.IV.Length) (byte 0)
         data.Read(ivBytes, 0, aes.IV.Length) |> ignore
         let decryptor = aes.CreateDecryptor(aes.Key, ivBytes)
         new CryptoStream(data, decryptor, CryptoStreamMode.Read)
 
     let decrypt (data : byte[]) (key : AesKey) : byte[] =
+        let key = decryptKeyBytes key
         let ms = new MemoryStream(data)
         use decryptionStream = createDecryptionStream key ms
         use sr = new StreamReader(decryptionStream)
         let stringData = sr.ReadToEnd()
         decryptionStream.Dispose()
+        zeroKey key
         Encoding.UTF8.GetBytes(stringData)
