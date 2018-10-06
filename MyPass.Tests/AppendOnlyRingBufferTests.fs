e397a049b1536c170f81dffcbecafaa3de8af756
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2018-10-06 02:30:19 +0100

    Finishing the append only ring buffer

---
+++ MyPass.Tests/AppendOnlyRingBufferTests.fs
@@ -0,0 +1,35 @@
+﻿namespace MyPass.Tests
+
+open NUnit.Framework
+open MyPass
+
+module AppendOnlyRingBufferTests =
+
+    [<Test>]
+    let ``When exact amount of data to fill buffer is inserted, then get returns correct list`` () =
+        let buffer = AppendOnlyRingBuffer.make 5
+        let fullBuffer =
+            buffer
+            |> AppendOnlyRingBuffer.add 1
+            |> AppendOnlyRingBuffer.add 2
+            |> AppendOnlyRingBuffer.add 3
+            |> AppendOnlyRingBuffer.add 4
+            |> AppendOnlyRingBuffer.add 5
+        let data = AppendOnlyRingBuffer.get fullBuffer
+        Assert.That(data, Is.EqualTo([|1;2;3;4;5|]))
+
+    [<Test>]
+    let ``When data overflows buffer, then get returns correct list`` () =
+        let buffer = AppendOnlyRingBuffer.make 5
+        let fullBuffer =
+            buffer
+            |> AppendOnlyRingBuffer.add 1
+            |> AppendOnlyRingBuffer.add 2
+            |> AppendOnlyRingBuffer.add 3
+            |> AppendOnlyRingBuffer.add 4
+            |> AppendOnlyRingBuffer.add 5
+            |> AppendOnlyRingBuffer.add 6
+            |> AppendOnlyRingBuffer.add 7
+            |> AppendOnlyRingBuffer.add 8
+        let data = AppendOnlyRingBuffer.get fullBuffer
+        Assert.That(data, Is.EqualTo([|4;5;6;7;8|]))
--- MyPass/CircularBuffer.fs
+++ MyPass/CircularBuffer.fs
@@ -1,12 +1,12 @@
 ﻿namespace MyPass
 
 /// Head always points at the next free slot
-type 'a CircularBuffer = {
+type 'a AppendOnlyRingBuffer = {
     Buffer : 'a option []
     Head : int
 }
 
-module CircularBuffer =
+module AppendOnlyRingBuffer =
 
     let make<'a> (capacity : int) =
         let (buffer : 'a option []) = Array.init capacity (fun i -> None)
@@ -16,7 +16,7 @@ module CircularBuffer =
             Head = 0
         }
 
-    let add (item : 'a) (buffer : 'a CircularBuffer) : 'a CircularBuffer =
+    let add (item : 'a) (buffer : 'a AppendOnlyRingBuffer) : 'a AppendOnlyRingBuffer =
         buffer.Buffer.[buffer.Head] <- Some item
 
         {
@@ -24,19 +24,20 @@ module CircularBuffer =
             Head = if buffer.Head = buffer.Buffer.Length - 1 then 0 else buffer.Head + 1
         }
 
-    let getAll (buffer : 'a CircularBuffer) : 'a [] =
-        let rec fill (oldBuffer : 'a option []) (startIndex : int) (currentIndex : int) (accIndex : int) (acc : 'a []) : 'a [] =
-            match oldBuffer.[currentIndex] with
-            | None -> acc
-            | Some v ->
-                acc.[accIndex] <- v
-                let nextIndex = if currentIndex + 1 = oldBuffer.Length then 0 else currentIndex + 1
-                if nextIndex = startIndex then
-                    acc
-                else
-                    fill oldBuffer startIndex nextIndex (accIndex + 1) acc
+    let get (buffer : 'a AppendOnlyRingBuffer) : 'a [] =
+        let rec fill (oldBuffer : 'a option []) (currentIndex : int) (accIndex : int) (acc : 'a []) : 'a [] =
+            match accIndex with
+            | _ when accIndex < 0 ->
+                acc
+            | _ ->
+                match oldBuffer.[currentIndex] with
+                | None -> failwith "impossible"
+                | Some v ->
+                    acc.[accIndex] <- v
+                    let nextIndex = if currentIndex - 1 < 0 then oldBuffer.Length - 1 else currentIndex - 1
+                    fill oldBuffer nextIndex (accIndex - 1) acc
 
         let filled = Array.sumBy (function | None -> 0 | Some _ -> 1) buffer.Buffer
         let returnBuffer = Array.init filled (fun _ -> Unchecked.defaultof<'a>)
         let startIndex = if buffer.Head - 1 < 0 then buffer.Buffer.Length - 1 else buffer.Head - 1
-        fill buffer.Buffer startIndex startIndex 0 returnBuffer
+        fill buffer.Buffer startIndex (returnBuffer.Length-1) returnBuffer
