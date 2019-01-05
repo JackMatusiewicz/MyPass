d5bc319cfa90b49aa5a3b99124c448901d515aee
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2019-01-05 17:30:37 +0000

    Adding retrying to clipboard clearing, also catching any exception that is thrown

--- MyPass.Clipboard/Clipboard.fs
+++ MyPass.Clipboard/Clipboard.fs
@@ -5,8 +5,19 @@ open System.Windows.Forms
 
 module Clipboard =
 
+    let rec private attemptToClearClipboard attempt =
+        if attempt > 2 then
+            printfn "Unable to clear the clipboard!"
+        else
+            try
+                Clipboard.Clear ()
+            with
+            | _ ->
+                printfn "Attempt #%d to clear the clipboard failed, retrying" (attempt + 1)
+                attemptToClearClipboard (attempt + 1)
+
     [<STAThread>]
     let timedStore (durationMs : int) (data : string) =
         Clipboard.SetText(data)
         System.Threading.Thread.Sleep(durationMs)
-        Clipboard.Clear()
+        attemptToClearClipboard 0
