e6172d0aa210a30d6fcebe437afc9cc1e093edc9
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2019-06-02 10:00:57 +0100

    Adding another PBT

--- MyPass.Tests/RoundTripTests.fs
+++ MyPass.Tests/RoundTripTests.fs
@@ -121,3 +121,38 @@ module RoundTripTests =
                 | MyPass.Result.Failure _ -> return true
                 | MyPass.Result.Success _ -> return false
         } |> Property.check' 100<tests>
+
+    [<Test>]
+    let ``Given a vault and all details incorrect, then the vault isn't decrypted`` () =
+        property {
+            let fs = new FileSystem ()
+            let currentDir = TestContext.CurrentContext.TestDirectory
+            match FileKey.read fs (Path.Combine (currentDir, "FileKey.fk")) with
+            | MyPass.Result.Failure _f ->
+               return false
+            | MyPass.Result.Success (FileKey fk) ->
+                let! fileKey =
+                    Gen.string (Range.linear 3 30) (Gen.char 'a' 'Z')
+                    |> Gen.filter ((<>) fk)
+                let! username =
+                    Gen.string (Range.linear 3 30) (Gen.char 'a' 'Z')
+                    |> Gen.filter ((<>) "test")
+                let! password =
+                    Gen.string (Range.linear 3 30) (Gen.char 'a' 'Z')
+                    |> Gen.filter ((<>) "test")
+                let passPhrase = SecureString.fromString password
+                let vaultPath = Path.Combine (currentDir, "TestVault.vt")
+
+                let fileKeyBytes = FileKey.toBytes (FileKey fileKey)
+                let key =
+                    MasterKey.make
+                        "Version1.0"
+                        fileKeyBytes
+                        username
+                        passPhrase
+
+                let manager = fs.File.ReadAllBytes vaultPath
+                match Vault.decrypt key manager with
+                | MyPass.Result.Failure _ -> return true
+                | MyPass.Result.Success _ -> return false
+        } |> Property.check' 100<tests>
